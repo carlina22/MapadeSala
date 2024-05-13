7d1eeaf50c34d63ea1ce83a2f254a9668d5437fd
Samuel Seven <sevenoffice777@gmail.com>
2024-05-13 14:29:07 -0300

    Add project files.

---
+++ MapadeSala/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapadeSala
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Professor());
+            Console.WriteLine("Hello World!!");
+        }
+    }
+}
